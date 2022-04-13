b64387628bd5b5c674a5fe3a869311c84a149f9e
Kevin <kfig21@gmail.com>
2022-04-13 17:04:28 -0400

    CORS error again

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const corsOptions = {
   origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
-  origin: true,
 };
 app.use(cors(corsOptions)); // Use this after the variable declaration
 app.options("*", cors());
