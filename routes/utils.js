a24f66f126fb9e7a01d0fff0dfae30266cb69a61
KevinFigenshu <kfig21@gmail.com>
2024-06-19 22:08:24 -0400

    remove console log

--- routes/utils.js
+++ routes/utils.js
@@ -5,7 +5,6 @@ const router = require("express").Router();
 // scrape job listing
 router.post("/scrape", async (req, res, next) => {
     const url = req.body.url
-    console.log('url', url)
     const data = await scrapeJobDetails(url)
     res.json(data)
 })
