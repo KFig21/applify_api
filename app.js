c00f5e673341261cbc23c96233f70b82c5acefb9
KevinFigenshu <kfig21@gmail.com>
2024-06-19 21:26:42 -0400

    app.js update for cors

--- app.js
+++ app.js
@@ -9,62 +9,61 @@ const createError = require("http-errors");
 const cors = require("cors");
 const path = require("path");
 
-// import routes
+// Import routes
 const authRouter = require("./routes/auth");
 const boardsRouter = require("./routes/boards");
 const jobsRouter = require("./routes/jobs");
 const utilsRouter = require("./routes/utils");
 
-// mongoDB setup
+// MongoDB setup
 const mongoDB = process.env.DB_CONNECTION_STRING;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// cors middleware
+// CORS middleware
 const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
+  origin: 'https://kfig21.github.io', // Update with your frontend URL
+  credentials: true,
+  optionSuccessStatus: 200
 };
-app.use(cors(corsOptions)); // Use this after the variable declaration
+app.use(cors(corsOptions));
 app.options("*", cors());
-//middleware
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-// use routes
+// Use routes
 app.use("/api/auth", authRouter);
 app.use("/api/boards", boardsRouter);
 app.use("/api/jobs", jobsRouter);
 app.use("/api/utils", utilsRouter);
 
-// view engine setup needed to keep from erroring out - ignore
+// View engine setup (if applicable)
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error"); // Ensure this view exists or modify accordingly
 });
 
-// const port = 3001;
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`);
+// Start server (if not using Heroku to set port)
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}!`);
 // });
 
 module.exports = app;
