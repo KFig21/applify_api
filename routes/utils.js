d81d8fbca72abcdc6b0ac161eff87ad6202ffae9
KevinFigenshu <kfig21@gmail.com>
2024-06-19 22:04:04 -0400

    Change request type

--- routes/utils.js
+++ routes/utils.js
@@ -3,8 +3,9 @@ const scrapeJobDetails = require("../utils/jobScraper");
 const router = require("express").Router();
 
 // scrape job listing
-router.get("/scrape/:url", async (req, res, next) => {
-    const url = req.params.url
+router.post("/scrape", async (req, res, next) => {
+    const url = req.body.url
+    console.log('url', url)
     const data = await scrapeJobDetails(url)
     res.json(data)
 })
--- utils/jobScraper.js
+++ utils/jobScraper.js
@@ -12,6 +12,7 @@ async function scrapeJobDetails(url) {
   chromeOptions.addArguments('--headless'); // Run in headless mode
   chromeOptions.addArguments('--no-sandbox'); // Bypass OS security model
   chromeOptions.addArguments('--disable-dev-shm-usage'); // Overcome limited resources problems
+  chromeOptions.addArguments("--disable-gpu")
   chromeOptions.addArguments(`user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36`);
   chromeOptions.addArguments('--disable-web-security'); // Disable web security
   chromeOptions.addArguments('--ignore-certificate-errors'); // Ignore certificate errors
