70aebdcf19d1e778c8b4ed9f8bf107a4c0a5f7d0
Kevin Figenshu <62974225+KFig21@users.noreply.github.com>
2024-10-15 13:53:54 +0000

    Update app.js

--- app.js
+++ app.js
@@ -16,29 +16,6 @@ const corsOptions ={
    optionSuccessStatus:200,
 }
 
-app.use((req, res, next) => {
-  // Set the Access-Control-Allow-Origin to allow your front-end origin
-  res.setHeader("Access-Control-Allow-Origin", "https://kfig21.github.io");
-  
-  // Specify the methods that are allowed
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  
-  // Specify the headers that can be sent in the requests
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  
-  // Allow credentials (e.g., cookies, authentication headers)
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  // If the request is an OPTIONS request (preflight), respond with 200
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  // Proceed to the next middleware or route handler
-  next();
-});
-
-
 app.use(cors(corsOptions))
 app.options("*", cors(corsOptions)); // Handle preflight requests
 
